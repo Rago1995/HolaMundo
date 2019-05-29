d79afebb798de7840e820965621d2b251b766a2d
DiamondDust <DiamondDust@192.168.1.71>
2019-05-28 23:07:42 -0500

    Agregue Hola mundo

--- src/com/pacienciamanu/sistema/Sistema.java
+++ src/com/pacienciamanu/sistema/Sistema.java
@@ -3,7 +3,7 @@ package com.pacienciamanu.sistema;
 public class Sistema {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Hola Mundo");
 
 	}
 
